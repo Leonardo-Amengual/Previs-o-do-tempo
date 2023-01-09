2eb838d06236f5f1879ae3a3952e18abb9271469
Leonardo Amengual <leonardo@santins.com.br>
2023-01-09 19:38:12 -0300

    funções e eventos do js criados

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,20 @@
 // Variáveis e seleção de elementos
-const apikey= "339530abff228ed357e750ba4745cf44";
+const apikey = "339530abff228ed357e750ba4745cf44";
 const apiCountryURL = "https://countryflagsapi.com/png/";
 
 const cityInput = document.querySelector("#cidade-input")
 const searchBtn = document.querySelector("#search")
 
 // Funções
-
+const showWeatherData = (city) => {
+    console.log(city)
+};
 
 // Eventos
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const city = cityInput.value;
+
+    showWeatherData(city);
+})

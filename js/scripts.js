6b48360a36d63944ec831ded6e08199cd5822bbb
Leonardo Amengual <leonardo@santins.com.br>
2023-01-09 20:07:28 -0300

    funcionalidade da api para buscar os dados climáticos da cidade

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,30 @@
 const apikey = "339530abff228ed357e750ba4745cf44";
 const apiCountryURL = "https://countryflagsapi.com/png/";
 
-const cityInput = document.querySelector("#cidade-input")
-const searchBtn = document.querySelector("#search")
+const cityInput = document.querySelector("#cidade-input");
+const searchBtn = document.querySelector("#search");
+
+const cityElement = document.querySelector("#cidade");
+const tempElement = document.querySelector("#temperatura span");
+const descElement = document.querySelector("#descricao");
+const weatherIconElement = document.querySelector("#icone-pais");
+const countryElement = document.querySelector("#pais");
+const umidityElement = document.querySelector("#humidade span");
+const windElement = document.querySelector("#vento span");
 
 // Funções
+const getWeatherdata = async (city) => {
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apikey}&lang=pt_br`;
+
+    const res = await fetch(apiWeatherURL);
+    const data = await res.json();
+
+    console.log(data);
+}
+
+
 const showWeatherData = (city) => {
-    console.log(city)
+    getWeatherdata(city)
 };
 
 // Eventos

1e338d8c70bbf91ca4f34015833e88354ce50a34
Leonardo Amengual <leonardo@santins.com.br>
2023-01-09 00:20:43 -0300

    começando a desenvolver o js da interface

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
-//339530abff228ed357e750ba4745cf44
+// Variáveis e seleção de elementos
+const apikey= "339530abff228ed357e750ba4745cf44";
+const apiCountryURL = "https://countryflagsapi.com/png/";
+
+const cityInput = document.querySelector("#cidade-input")
+const searchBtn = document.querySelector("#search")
+
+// Funções
+
+
+// Eventos
